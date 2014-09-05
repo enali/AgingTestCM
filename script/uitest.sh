076622abd48ca2be77bce992af12f10eebd8eb0a
enali <enalix@163.com>
2014-09-05 07:54:30 -0700

    rm ant build

--- script/uitest.sh
+++ script/uitest.sh
@@ -5,34 +5,23 @@
 
 SDK_PATH=$HOME/adt/sdk
 ADB_PATH=$SDK_PATH/playform-tools
+ANDROID_PATH=$SDK_PATH/tools
+PROJ_NAME=AgingTestCM
 
-# you may need to create/update build.xml
-# ANDROID_PATH=$SDK_PATH/tools
-# $ANDROID_PATH/android create uitest-project -n $PROJ_NAME -t 1 -p $PROJ_PATH
 
 ####    run test   ####
-# git clone at $HOEM/workspace/
-PROJ_NAME=AgingTestCM
-PROJ_PATH=$HOME/workspace/$PROJ_NAME 
 if [ "$1" = "run" ]; then
-    if $(which ant >/dev/null 2>&1); then
-        cd $PROJ_PATH && ant build
-    elif
-        # eclipse config Ant_builder:
-        # right click project, Properties->Builders->New(ant builder). if you clone from github, you may not need it
-        # set Name, set Buildfile( build.xml ), set Base directory, set Arguments( build ).
-        # right click project's build.xml, choose Ant build
-        echo "make sure you has run ant build in eclipse"
-    fi
-
-    $ADB_PATH/adb push $PROJ_PATH/bin/${PROJ_NAME}.jar /data/local/tmp/
+    $ANDROID_PATH/android create uitest-project -n $PROJ_NAME -t 1 -p ../
+    cd ../
+    ant build
+    $ADB_PATH/adb push ../bin/${PROJ_NAME}.jar /data/local/tmp/
     $ADB_PATH/adb shell uiautomator runtest ${PROJ_NAME}.jar
 fi
 ####    end test   ####
 
 ####    init test   ####
 UITEST_PHONE_PATH=/sdcard/uitest
-UITEST_HOST_PATH=$PROJ_PATH/uitest
+UITEST_HOST_PATH=../uitest
 if [ "$1" = "init" ]; then
     $ADB_PATH/adb shell rm -rf $UITEST_PHONE_PATH
     $ADB_PATH/adb push $UITEST_HOST_PATH $UITEST_PHONE_PATH
